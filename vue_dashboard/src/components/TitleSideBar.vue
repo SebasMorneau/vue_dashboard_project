918b17bd852626e03f6dbb22c27b826206216a98
Sebastien <morneausebastien@gmail.com>
2021-05-02 14:14:12 -0400

    HTML Structure is done

--- vue_dashboard/src/components/TitleSideBar.vue
+++ vue_dashboard/src/components/TitleSideBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-row :align="align" no-gutters style="height: 70px">
+    <v-row :align="align" no-gutters>
       <v-col cols="6">
         <v-list-item>
           <v-list-item-content>
@@ -19,7 +19,7 @@
     </v-row>
 
     <v-divider></v-divider>
-    <v-row :align="align" no-gutters style="height: 60px">
+    <v-row :align="align" no-gutters>
       <v-list-item>
         <v-list-item-content>
           <v-col cols="12" sm="5">
@@ -37,15 +37,19 @@
       </v-list-item>
     </v-row>
     <v-row :align="align" no-gutters>
-      <v-col cols="6" class="d-flex align-center flex-column start-button"
-        >Ouvert</v-col
+      <v-col cols="6"
+        ><div class="d-flex align-center flex-column start-button active">
+          Ouvert
+        </div></v-col
       >
-      <v-col cols="6" class="d-flex align-center flex-column start-button"
-        >Fermé</v-col
+      <v-col cols="6"
+        ><div class="d-flex align-center flex-column start-button disable">
+          Fermé
+        </div></v-col
       >
     </v-row>
 
-    <v-row :align="align" no-gutters style="height: 40px">
+    <v-row :align="align" no-gutters>
       <v-col cols="12">
         <v-list-item class="grow">
           <v-list-item-avatar color="grey darken-3">
@@ -62,6 +66,18 @@
         </v-list-item>
       </v-col>
     </v-row>
+
+    <v-divider></v-divider>
+    <div v-for="(sidebarItem, i) in sidebarItems" :key="i">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="sidebarItems">
+            {{ sidebarItem }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+    </div>
   </div>
 </template>
 
@@ -69,16 +85,42 @@
 export default {
   name: "TitleSideBar",
   data() {
-    return {};
+    return {
+      sidebarItems: [
+        "Informations",
+        "Honoraires",
+        "Tâches",
+        "Dépenses",
+        "Factures",
+        "Fichiers",
+        "Export",
+        "Paramètres",
+      ],
+    };
   },
 };
 </script>
 
 <style scoped>
+* {
+  background-color: #f2f2f2;
+}
 .date {
   font-size: 9px;
 }
 .start-button {
-  border: 1px solid grey;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  width: 100%;
+}
+.disable {
+  background-color: gainsboro;
+}
+.active {
+  background-color: white;
+}
+.sidebarItems {
+  padding-top: 12px;
+  padding-bottom: 12px;
 }
 </style>
